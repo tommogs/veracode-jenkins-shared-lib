f92205d0e740f4a3bcb64b3e27afea9ad9f13d12
Chris Campbell <chris@ctcampbell.com>
2020-03-09 14:10:35 +0000

    Rename

--- vars/uploadAndScan.groovy
+++ vars/uploadAndScan.groovy
@@ -1,3 +1,3 @@
-def uploadandscan(Stirng id, String key, String appname, String filepath) {
+def call(String id, String key, String appname, String filepath) {
     sh "java -jar veracode-wrapper.jar -action uploadandscan -vid ${id} -vkey ${key} -appname ${appname} -createprofile false -version 'test1234' -filepath ${filepath}"
 }
