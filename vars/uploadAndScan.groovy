8f10d65c2c25c96a51081c52ce5076157594db7f
Chris Campbell <chris@ctcampbell.com>
2020-03-09 14:18:40 +0000

    Update uploadAndScan.groovy

--- vars/uploadAndScan.groovy
+++ vars/uploadAndScan.groovy
@@ -1,3 +1,3 @@
 def call(String id, String key, String appname, String filepath) {
-    sh "java -jar veracode-wrapper.jar -action uploadandscan -vid ${id} -vkey ${key} -appname ${appname} -createprofile false -version 'test1234' -filepath ${filepath} -sandboxname shared-lib-test -createsandbox true"
+    sh 'java -jar veracode-wrapper.jar -action uploadandscan -vid ${id} -vkey ${key} -appname ${appname} -createprofile false -version "commit ${env.GIT_COMMIT[0..6]} build ${env.BUILD_NUMBER}" -filepath ${filepath} -sandboxname shared-lib-test -createsandbox true'
 }
